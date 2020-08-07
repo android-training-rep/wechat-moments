9750459d2c460e046c349c4051fc12a94440f3aa
Liao Xueqing <xueqingliao@foxmail.com>
2020-08-07 23:26:00 +0800

    在页面中显示user信息

--- app/src/main/java/com/example/wechatmoments/MainActivity.java
+++ app/src/main/java/com/example/wechatmoments/MainActivity.java
@@ -4,6 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 import com.example.wechatmoments.model.User;
 import com.example.wechatmoments.repository.UserRepository;
 import com.example.wechatmoments.utils.HttpUtil;
@@ -13,6 +17,8 @@ public class MainActivity extends AppCompatActivity {
     public final String TAG = this.getClass().getName();
     private HttpUtil httpUtil = new HttpUtil();
     private UserViewModel userViewModel;
+    ImageView profileImage, avatar;
+    TextView nick;
 
 
     @Override
@@ -20,18 +26,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        profileImage = findViewById(R.id.profile_image);
+        avatar = findViewById(R.id.avatar);
+        nick = findViewById(R.id.nick);
+
         userViewModel = obtainViewModel();
         Observer userObserver = new Observer<User>(){
             @Override
             public void onChanged(User user) {
-                // todo 把user绑定到界面
-                System.out.println("-------------user:" + user.getUsername());
+                System.out.println("-------------user:" + user.getNick());
+                // todo profile-image 转换
+                Glide.with(profileImage).load(user.getProfileImage()).into(profileImage);
+                Glide.with(avatar).load(user.getAvatar()).into(avatar);
+                nick.setText(user.getNick());
             }
         };
         userViewModel.getUser().observe(this, userObserver);
 
         userViewModel.loadUserInfo();
     }
+
 
     private UserViewModel obtainViewModel() {
         UserRepository userRepository = new UserRepository(httpUtil);
--- app/src/main/java/com/example/wechatmoments/utils/UrlUtil.java
+++ app/src/main/java/com/example/wechatmoments/utils/UrlUtil.java
@@ -2,4 +2,5 @@ package com.example.wechatmoments.utils;
 
 public class UrlUtil {
     public static final String User_Info_Url = "https://twc-android-bootcamp.github.io/fake-data/data/weixin/profile.json";
+    public static final String Tweets_url= "https://twc-android-bootcamp.github.io/fake-data/data/weixin/tweets.json";
 }
