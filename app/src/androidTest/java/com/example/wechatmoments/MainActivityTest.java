de278083c11e7c87a420ab3875b7c0ccfa19f4ac
Liao Xueqing <xueqingliao@foxmail.com>
2020-08-08 21:53:53 +0800

    抽取部分宽高到dimen文件

---
+++ app/src/androidTest/java/com/example/wechatmoments/MainActivityTest.java
@@ -0,0 +1,21 @@
+package com.example.wechatmoments;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void should_show_nick_name_correct() {
+        // todo add test code
+    }
+}
